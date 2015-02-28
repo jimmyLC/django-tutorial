94dfe842b951efdb9f8ec11ce25bd04e219dd3ec
Jimmy <acsd68500@gmail.com>
2015-02-28 18:07:28 +0800

    full

---
+++ mysite/trips/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from trips.models import Post
+
+def hello_world(request):
+  return render(request,
+                'hello_world.html',
+                {'current_time': datetime.now()})
+
+def home(request):
+  post_list = Post.objects.all()
+  return render(request,
+                'home.html',
+                {'post_list': post_list})
+
+def post_detail(request, id):
+  post = Post.objects.get(id=id)
+  return  render(request, 'post.html', {'post': post})
